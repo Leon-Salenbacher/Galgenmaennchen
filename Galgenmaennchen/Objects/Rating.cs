24a6c7f59d1b36d8f4c8513f3d8ba2f2905cd639
Leon-BoU <leon@salenbacher.com>
2022-12-14 10:57:42 +0100

    Object User and Rating created

--- Galgenmaennchen/Objects/Rating.cs
+++ Galgenmaennchen/Objects/Rating.cs
@@ -8,5 +8,47 @@ namespace Galgenmaennchen.Objects
 {
     class Rating
     {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int versuche;
+
+        public int Versuche
+        {
+            get { return versuche; }
+            set { versuche = value; }
+        }
+
+        private string word;
+
+        public string Word
+        {
+            get { return word; }
+            set { word = value; }
+        }
+
+        public Rating()
+        {
+
+        }
+
+        public Rating(int versuche, string word)
+        {
+            this.versuche = versuche;
+            this.word = word;
+        }
+
+        public Rating(int id, int versuche, string word)
+        {
+            this.versuche = versuche;
+            this.word = word;
+            this.id = id;
+        }
+
     }
 }
--- Galgenmaennchen/Objects/User.cs
+++ Galgenmaennchen/Objects/User.cs
@@ -8,5 +8,46 @@ namespace Galgenmaennchen.Objects
 {
     class User
     {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User()
+        {
+
+        }
+
+        public User(String name, String password, int Id)
+        {
+            this.name = name;
+            this.password = password;
+        }
+        public User(int id, String name, String password, int Id)
+        {
+            this.name = name;
+            this.password = password;
+            this.id = id;
+        }
+
     }
 }
